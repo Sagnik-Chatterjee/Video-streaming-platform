e25bf884372a31eafee96b0fb1c4009314e2748b
Sagnik-Chatterjee <164457905+Sagnik-Chatterjee@users.noreply.github.com>
2026-06-08 23:41:02 +0530

    Made Changes in index.js in db

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -3,7 +3,13 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB=async()=>{
     try{
-        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        const uri = process.env.MONGODB_URI;
+const connectionString = uri.includes('?') 
+    ? uri.replace('/?', `/${DB_NAME}?`) 
+    : `${uri}/${DB_NAME}`;
+
+const connectionInstance = await mongoose.connect(connectionString);
+console.log("Mongodb connected")
     }catch(error){
         console.log("Mongpodb Connection error",error)
         process.exit(1)
