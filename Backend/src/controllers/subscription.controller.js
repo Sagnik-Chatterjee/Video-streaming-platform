454740385e97e1e0173b8f1d0ba9e82b9c46b265
Sagnik-Chatterjee <164457905+Sagnik-Chatterjee@users.noreply.github.com>
2026-06-08 23:06:31 +0530

    Solved some errors

--- Backend/src/controllers/subscription.controller.js
+++ Backend/src/controllers/subscription.controller.js
@@ -1,7 +1,7 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import { Subscription } from "../models/subscription.model.js"
 import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse"
+import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
 
